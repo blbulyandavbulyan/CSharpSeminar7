1faaeb622b6fd85759e0371b2c636aa37212cfde
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-16 21:13:59 +0300

    task 46 solved in seminar7 project

--- seminar7/Program.cs
+++ seminar7/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//demo task 46
+int[,] generateRandom2DArray(int columns, int rows, int minRandomNumber = 0, int maxRandomNumber = 100)
+{
+    int[,] result = new int[columns, rows];
+    for(int i = 0; i < columns; i++)
+    {
+        for(int j = 0; j < rows; j++)
+        {
+            result[i, j] = Random.Shared.Next(minRandomNumber, maxRandomNumber + 1);
+        }
+    }
+    return result;
+}
+void Print2DArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+int[,] randomArray = generateRandom2DArray(20, 20);
+Print2DArray(randomArray);
