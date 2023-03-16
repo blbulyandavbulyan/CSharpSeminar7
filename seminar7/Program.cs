63ce6f6e03c0dfbdacadd4e4abe5314086f4087c
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-16 21:37:34 +0300

    task 48 solved

--- seminar7/Program.cs
+++ seminar7/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
 //demo task 46
+//эта функция генерирует двумерный массив заданных размеров с случайными элементами принадлежащих промежутку [0; 100]
 int[,] generateRandom2DArray(int columns, int rows, int minRandomNumber = 0, int maxRandomNumber = 100)
 {
     int[,] result = new int[columns, rows];
---
+++ task48/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m + n.Выведите полученный массив на экран.
+//m = 3, n = 4.
+//0 1 2 3
+//1 2 3 4
+//2 3 4 5
+int getInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+}
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j]} ");
+        Console.WriteLine();
+    }
+}
+int m = getInt("Введите количество строк: ");
+int n = getInt("Введите количество столбцов: ");
+int[,] arr = new int[m, n];
+FillArray(arr);
+Print2DArray(arr);
